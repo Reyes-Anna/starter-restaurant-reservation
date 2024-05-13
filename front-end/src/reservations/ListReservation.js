ff0ae7cd1b669c0e35ebe0e77e8f1e9bea535b02
Anna Reyes <avreyes703@outlook.com>
2024-05-13 19:33:13 -0400

    updated loadRes to history.go(0)

--- front-end/src/reservations/ListReservation.js
+++ front-end/src/reservations/ListReservation.js
@@ -10,7 +10,7 @@ function ListReservations({ reservation, setReservationsError, loadDashboard}) {
       const message = "Do you want to cancel this reservation? This cannot be undone.";
       if (window.confirm(message)) {
         updateStatus(reservation.reservation_id, "cancelled")
-          .then(() => loadDashboard())
+          .then(() =>history.go(0))
           .catch(setReservationsError);
       }
     };
